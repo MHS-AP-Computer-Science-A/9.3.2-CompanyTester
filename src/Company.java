9eefc88228479c2ade691a129214060c87ec10d1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-02-21 10:46:23 -0500

    Update Company.java

--- src/Company.java
+++ src/Company.java
@@ -1,40 +1,36 @@
 public class Company {
-    
+
     private String name;
     private String streetAddress;
     private String city;
     private String state;
 
-    // Set missing values to null
-    public Company(String name){
-        
-    }
-    
-
-    public Company(String name, String streetAddress, String city, String state){
-       
+    // Complete the 1-parameter constructor (Set missing values to null)
+    public Company(String name) {
+   
     }
 
-    public String getName(){
-      
+    // Complete the 4-parameter constructor
+    public Company(String name, String streetAddress, String city, String state) {
+   
     }
 
+    // Complete the getAddress method that returns an address formatted as shown
     /**
-    * Example output:
-    * 123 Main St
-    * Springfield, OH
-    */
-    public String address(){
-       
+     * 123 Main St
+     * Springfield, OH
+     */
+    public String getAddress() {
+        return "";
     }
 
+    // Complete the toString method to return a String as shown
     /**
-    * Example output:
-    * Widget Company
-    * 123 Main St
-    * Springfield, OH
-    */
-    public String toString(){
-       
+     * Widget Company
+     * 123 Main St
+     * Springfield, OH
+     */
+    public String toString() {
+        return "";
     }
 }
